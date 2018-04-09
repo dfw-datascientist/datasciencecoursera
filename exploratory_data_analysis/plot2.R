cba9a50430aefbb9670e93869cbfa63500f91666
Jithu Lakshmikanth <jlakshmikanth@jlakshmikanthmbp.averocorp.averoinc.com>
2018-04-08 23:36:59 -0500

    All files needed for assignment 1

---
+++ exploratory_data_analysis/plot2.R
@@ -0,0 +1,3 @@
+png(file = "plot2.png")
+with(hpc_set, plot(DateTime, Global_active_power, ylab = "Global Active Power (kilowatts)", xlab = "", type = "l"))
+dev.off()
---
+++ exploratory_data_analysis/plot3.R
@@ -0,0 +1,7 @@
+png(file = "plot3.png")
+plot(hpc_set$DateTime, hpc_set$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+points(hpc_set$DateTime, hpc_set$Sub_metering_1, type = "l")
+points(hpc_set$DateTime, hpc_set$Sub_metering_2, type = "l", col = "red")
+points(hpc_set$DateTime, hpc_set$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
